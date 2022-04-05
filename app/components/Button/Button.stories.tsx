be1e1610a5e80a60fbbeb33f0bd3f3804b960550
KlaraGajaszek <klara.gajaszek@coderscrew.pl>
2022-04-05 13:14:41 +0200

    add storybook

--- app/components/Button/Button.stories.tsx
+++ app/components/Button/Button.stories.tsx
@@ -1,11 +1,25 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Button } from "./Button";
+import { ComponentMeta, Story } from "@storybook/react";
+
+import { Button, ButtonProps } from "./Button";
+import { action } from "@storybook/addon-actions";
 
 export default {
-  text: "Button",
+  title: "Components/Button",
   component: Button,
+  argTypes: {
+    onClick: { action: "click" },
+    children: { defaultValue: "Default Text" },
+  },
 } as ComponentMeta<typeof Button>;
 
-export const Primary: ComponentStory<typeof Button> = () => (
-  <Button text="ddd" />
-);
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  variant: "secondary",
+  children: "Secondary",
+  onClick: action("secondary click"),
+};
--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,15 +1,17 @@
 import clsx from "clsx";
 
-export type ButtonProps = {
+export type ButtonInnerProps = {
   variant?: "primary" | "secondary";
   children: React.ReactNode | React.ReactNode[];
 };
+
+export type ButtonProps = ButtonInnerProps & JSX.IntrinsicElements["button"];
 
 const getClassName = ({ className }: { className?: string }) => {
   return clsx("text-sm font-small", className);
 };
 
-const ButtonInner = ({ children, variant }: ButtonProps) => {
+const ButtonInner = ({ children, variant }: ButtonInnerProps) => {
   return (
     <div
       className={clsx("rounded-sm py-1 px-5 text-white", {
@@ -27,7 +29,7 @@ export const Button = ({
   className,
   children,
   ...buttonProps
-}: ButtonProps & JSX.IntrinsicElements["button"]) => {
+}: ButtonProps) => {
   return (
     <button {...buttonProps} className={getClassName({ className })}>
       <ButtonInner variant={variant}>{children}</ButtonInner>
